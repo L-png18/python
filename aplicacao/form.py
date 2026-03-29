d810024adfc50262566d60d0e1673431c8c485f5
Leonardo Rodrigues <81760371+L-png18@users.noreply.github.com>
2026-03-29 13:27:31 -0300

    atualizado estetiamente

--- aplicacao/form.py
+++ aplicacao/form.py
@@ -4,10 +4,10 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Perfil
 
 class UsuarioForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='E-mail',widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(label='Nome de usuário',widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='Senha',widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Confirmar senha',widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
